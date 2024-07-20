108ea12003741db89a1ff4351492e150d7449f69
hardwork9047 <tomatosimple1104@gmail.com>
2024-07-20 17:02:56 +0900

    modify hello.yml

--- hello.sh
+++ hello.sh
@@ -9,3 +9,7 @@ echo "Hostname: $(hostname)"
 
 # IPアドレスを表示
 echo "IP Address: $(hostname -I | awk '{print $1}')"
+
+# リポジトリの最新状態を取得
+echo "Pulling latest changes from git repository"
+git pull origin main
